9df45dd185d1b7a8268d2d089de316d0afba8bbe
Khun174 <22523264@students.uii.ac.id>
2024-06-19 19:53:56 +0700

    dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>UI/UX</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
    <div class="container">
@@ -14,7 +15,7 @@
            
          <div class="top">
            <div class="logo">
-             <h2>DD <span class="danger">DearDay</span> </h2>
+             <h2><img src="../img/colored.png" alt=""> <span class="danger">DearDay</span> </h2>
            </div>
            <div class="close" id="close_btn">
             <span class="material-symbols-sharp">
@@ -25,42 +26,33 @@
          <!-- end top -->
           <div class="sidebar">
 
-            <a href="#">
+            <a href="#" class="active">
               <span class="material-symbols-sharp">grid_view </span>
-              <h3>Dashbord</h3>
+              <h3>Your Activity</h3>
            </a>
-           <a href="#" class="active">
+           <a href="../dashboard/journal_history.php" >
+              <span class="material-symbols-sharp">library_books </span>
+              <h3>Journal History</h3>
+           </a>
+           <a href="../dashboard/mood_tracker.php">
+              <span class="material-symbols-sharp">sentiment_satisfied </span>
+              <h3>Mood Tracker</h3>
+           </a>
+           <a href="../dashboard/expression_tracker.php">
+              <span class="material-symbols-sharp">ar_on_you </span>
+              <h3>Expression Tracker</h3>
+           </a>
+           <a href="../dashboard/quiz_history.php">
+              <span class="material-symbols-sharp">abc </span>
+              <h3>Quiz History</h3>
+           </a>
+           <a href="../dashboard/profile.php">
               <span class="material-symbols-sharp">person_outline </span>
-              <h3>custumers</h3>
+              <h3>Profile</h3>
            </a>
-           <a href="#">
-              <span class="material-symbols-sharp">insights </span>
-              <h3>Analytics</h3>
-           </a>
-           <a href="#">
-              <span class="material-symbols-sharp">mail_outline </span>
-              <h3>Messages</h3>
-              <span class="msg_count">14</span>
-           </a>
-           <a href="#">
-              <span class="material-symbols-sharp">receipt_long </span>
-              <h3>Products</h3>
-           </a>
-           <a href="#">
-              <span class="material-symbols-sharp">report_gmailerrorred </span>
-              <h3>Reports</h3>
-           </a>
-           <a href="#">
-              <span class="material-symbols-sharp">settings </span>
-              <h3>settings</h3>
-           </a>
-           <a href="#">
-              <span class="material-symbols-sharp">add </span>
-              <h3>Add Product</h3>
-           </a>
-           <a href="#">
+           <a href="../interface/interface.php">
               <span class="material-symbols-sharp">logout </span>
-              <h3>logout</h3>
+              <h3>Main Menu</h3>
            </a>
              
 
